20f3f167841d2f240c8b76232e6875d8c79c8044
burgossrodrigo <rodrigoburgos@live.com>
2026-04-10 18:03:20 +0000

    chore: add missing example folder

---
+++ example/metro.config.js
@@ -0,0 +1,16 @@
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+
+const config = {
+  watchFolders: [root],
+  resolver: {
+    nodeModulesPaths: [
+      path.resolve(__dirname, 'node_modules'),
+      path.resolve(root, 'node_modules'),
+    ],
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
